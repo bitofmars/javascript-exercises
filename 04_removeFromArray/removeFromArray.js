77b0fb4e31f489bd023ad31a0dba9efb1f28e95b
Martina Peluso <bitfmars@gmail.com>
2024-07-28 19:32:06 -0300

    Solve the fourth JS exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
-};
+/* My original solution, but it failed one test.
+
+const removeFromArray = function(array, ...args) {
+    for (let arg of args){
+        let index = array.indexOf(arg);
+        if (index !== -1){
+            array.splice(index, 1);
+        }
+    }
+    return array;
+}; */
+
+// The actual solution, that's easier
+
+const removeFromArray = function (array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    return newArray;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;

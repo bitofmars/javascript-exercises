d01269b0d241a3c5193a1eb43bc4fcc35baa18d0
Martina Peluso <bitfmars@gmail.com>
2024-07-28 18:59:59 -0300

    Solve the third JS exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // 1. Every character of the string is put inside an array
+    // 2. The array is reversed
+    // 3. The array elements get concatenated and become a string
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line

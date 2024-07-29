50328cd497f173823aefd0f038fa3d659dad1897
Martina Peluso <bitfmars@gmail.com>
2024-07-29 18:17:04 -0300

    Solve the seventh JS exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let converted = (fahrenheit - 32) / 1.8;
+  if (converted === 0){
+    return converted;
+  }else{
+    return +converted.toFixed(1);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let converted = (celsius * 1.8) + 32;
+  if (converted === 0){
+    return converted;
+  }else{
+    return +converted.toFixed(1);
+  }
 };
 
 // Do not edit below this line
